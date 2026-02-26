16e8123f8aa6dee8d5b0e92eeec09792ec5720ba
Visual Lab <visuallabme2025@gmail.com>
2026-02-26 17:05:47 +0400

    swiperjs at banner,testimonials and gallery of home page

---
+++ src/components/home/WhyUs.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Whyus(){
+    return(
+        <section className="bg-gray-200 p-16">
+            <div className="max-w-6xl mx-0">
+                <div className="grid">
+                    <div className="">
+                        icon
+                        <h2>Expert Team</h2>
+                        <p>Highly skilled designers, architects, and project managers.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
